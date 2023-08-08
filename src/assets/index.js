b3167fa0dbddbfd92dd612727cafccfdfb8fa284
Mukhtar <myl772ng@gmail.com>
2023-08-08 15:01:39 +0100

    chnage

--- src/assets/index.js
+++ src/assets/index.js
@@ -23,9 +23,11 @@ import twitter from "./twitter.svg";
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
+import bawesLog from './bawesLog.png';
 
 export {
   airbnb,
+  bawesLog,
   bill,
   binance,
   card,
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import {close, logo, menu} from '../assets';
+import {close, logo, bawesLog, menu} from '../assets';
 import { navLinks } from '../constants'
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false)
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt='hoobank' className='w-[124px] h-[32px]'/>
+      <img src={bawesLog} alt='hoobank' className='w-[124px] h-[32px]'/>
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
       {
         navLinks.map((nav, index) =>(
